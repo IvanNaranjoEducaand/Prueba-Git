b68d112a8e1c4c212f9a9a10560d96cba4377757
IvanNaranjoEducaand <inarmar@g.educaand.es>
2024-02-23 12:39:41 +0100

    Actualización con comentarios

--- Figura.java
+++ Figura.java
@@ -1,4 +1,8 @@
+/*
+* Representa la estructura básica de figuras geometricas
+* Las subclases deben implementar los metodos
+*/
 public abstract class Figura(){
-	public abstract double area();
-	public abstract double perimetro();
+	public abstract double area(); /*Devuelve la área de la figura*/
+	public abstract double perimetro(); /*Devuelve el perímetro de la figura*/
 }
